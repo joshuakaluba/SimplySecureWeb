e747f26c555fa8b8dc3533225971f62e43973109
Joshua Kaluba <joshkaluba@gmail.com>
2019-01-08 20:51:30 -0500

    Created enviroments sample settings files for both prod and dev environments

---
+++ src/environments/environment.prod.sample.ts
@@ -0,0 +1,5 @@
+//change the apiUrl to point to your dev api and then rename this file to `environment.prod.ts`
+export const environment = {
+  production: false,
+  apiUrl: "https://www.example.com"
+};
---
+++ src/environments/environment.sample.ts
@@ -0,0 +1,5 @@
+//change the apiUrl to point to your dev api and then rename this file to `environment.ts`
+export const environment = {
+  production: false,
+  apiUrl: "http://localhost:51606"
+};
