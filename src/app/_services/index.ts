b52f711bdc850e95978ee086326221d4fd7cd855
Joshua Kaluba <joshkaluba@gmail.com>
2019-01-18 20:13:52 -0500

    Created index file listing all services

---
+++ src/app/_services/index.ts
@@ -0,0 +1,6 @@
+﻿export * from './alert.service';
+export * from './user.service';
+export * from './location.service';
+export * from './base.authenticated.service';
+export * from './authentication.service';
+export * from './module.service';
