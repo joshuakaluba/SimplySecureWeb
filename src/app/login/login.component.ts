5aab694febc65775fe24d78f6f0b2baf2f974992
Joshua Kaluba <joshkaluba@gmail.com>
2019-01-22 21:01:58 -0500

    Created login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { NgForm } from "@angular/forms";
+import { UserService, AuthenticationService, AlertService } from "../_services";
+
+@Component({ templateUrl: "login.component.html" })
+export class LoginComponent implements OnInit {
+  invalidInput: boolean;
+  loading: boolean = false;
+  errorMessage: string;
+  returnUrl: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private alertService: AlertService
+  ) {}
+  ngOnInit() {
+    if (this.authenticationService.isUserAuthenticated()) {
+      this.router.navigate(["/locations"]);
+      return;
+    }
+
+    this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/";
+  }
+
+  login(form: NgForm) {
+    this.loading = true;
+    this.userService.login(form.value).subscribe(
+      response => {
+        this.alertService.success("Login successful", true);
+        this.loading = false;
+
+        let user = <any>response;
+
+        localStorage.setItem("user", JSON.stringify(user));
+        localStorage.setItem("simplyJwt", user.accessToken);
+        this.router.navigate(["/locations"]);
+      },
+      err => {
+        console.log(err);
+        this.errorMessage = err.error.message;
+        this.loading = false;
+        this.invalidInput = true;
+      }
+    );
+  }
+}
